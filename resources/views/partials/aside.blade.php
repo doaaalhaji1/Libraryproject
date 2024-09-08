fcdcf61ef5bfe91d875a529c6822b63b954be3de
Mustafa-Ghanoom <Mustafaghanoom@gmail.com>
2024-09-08 12:33:59 -0800

    edit

--- resources/views/partials/aside.blade.php
+++ resources/views/partials/aside.blade.php
@@ -2,30 +2,34 @@
 
  <style>
     .sidebar {
-     /* background-color: #343a40; */
      display: flex;
      flex-direction: column;
      text-align: center;
      justify-content: center;
  }
 
- .button a {
+
+ .button .centered-link {
      color: white;
-     display: block;
-     padding: 10px 20px;
-     margin: 5px 0;
      font-size: 18px;
-     text-decoration: none;
-     border-radius: 10px;
-     text-align: left;
+     border-radius: 25px;
+    text-decoration: none;
+    padding: 9px 55px;
+    margin-top:5px;
+    display: block;
+
  }
- .sidebar a:hover {
+
+ .button .centered-link:hover {
      background-color: #184cd0;
-     text-decoration: none;
+
+
  }
 
  .sidebar a i {
      margin-right: 15px;
+
+
  }
 /* ---------------------------------------- */
 
@@ -35,21 +39,17 @@
     margin: 10px;
     background-color: #007bff;
     color: white;
-    border-radius: 5px;
+    border-radius: 25px;
     text-decoration: none;
     transition: transform 0.2s, background-color 0.3s;
     animation: pulse 2s infinite;
 }
 
 .animated-button:hover {
-    background-color: #0056b3;
     transform: scale(1.1);
     box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
 }
 
-.animated-button:active {
-    transform: scale(0.95); /* تقليل الحجم عند الضغط */
-}
 
 @keyframes pulse {
     0%, 100% {
@@ -76,11 +76,12 @@
                     Returned Books
                 </a>
 
-                <div class="button">
-
-                </div>
-                 {{-- <a href={{route("users.create")}}><i class="bi bi-plus-circle-fill"></i>{{  __ ('messages.Add_User') }}</a> --}}
-                 {{-- <a href={{route("categories.create")}}><i class="bi bi-plus-circle-fill"></i> {{  __ ('messages.Add_Category') }}</a> --}}
+                <div class="button mt-4">
+                    <a href="{{ route('users.create') }}" class="centered-link"> <i class="bi bi-plus-circle-fill"></i> Create User</a>
+                    <a href="{{ route('users.create') }}" class="centered-link"> <i class="bi bi-plus-circle-fill"></i> Create User</a>
+                    <a href="{{ route('users.create') }}" class="centered-link"> <i class="bi bi-plus-circle-fill"></i> Create User</a>
+                    <a href="{{ route('users.create') }}" class="centered-link"> <i class="bi bi-plus-circle-fill"></i> Create User</a>
+                    <a href="{{ route('users.create') }}" class="centered-link"> <i class="bi bi-plus-circle-fill"></i> Create User</a>
                 </div>
 
 
